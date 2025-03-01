4374816c41e1881487d9ed0085a4e4c930708d65
amitdazn <122984881+amitdazn@users.noreply.github.com>
2025-03-01 13:47:30 +0530

    fix: contract by id API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const {getProfile} = require('./middleware/getProfile');
+const { Sequelize } = require('sequelize');
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
@@ -12,10 +13,22 @@ app.set('models', sequelize.models)
  * @returns contract by id
  */
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
+    const {Contract} = req.app.get('models');
+    const {id} = req.params;
+    const profileId = req.profile.id;
+
+    const contract = await Contract.findOne({
+        where: {
+            id,
+            [Sequelize.Op.or]: [
+                {ClientId: profileId},
+                {ContractorId: profileId}
+            ]
+        }
+    });
+    
+    if(!contract) return res.status(404).end();
+
+    res.json(contract);
 })
 module.exports = app;
